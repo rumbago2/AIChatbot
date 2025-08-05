cf5c6cc79d6167bab206ccf6541678d43d4fcd22
rumbago2@gmail.com <rumbago2@gmail.com>
2025-08-05 00:40:28 +0000

    Created Form1.RowTemplate2; Edited Form1; Deleted Form1.RowTemplate1

--- client_code/Form1/RowTemplate2/__init__.py
+++ client_code/Form1/RowTemplate2/__init__.py
@@ -1,15 +1,14 @@
-from ._anvil_designer import RowTemplate1Template
+from ._anvil_designer import RowTemplate2Template
 from anvil import *
 import anvil.server
 
 
-class RowTemplate1(RowTemplate1Template):
+class RowTemplate2(RowTemplate2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # Check if self.item is set (Anvil sets this automatically for each row)
+    print(self.item)  # Should print your dictionary to console for each row
     if self.item:
       self.label_session.text = self.item.get('session_name', 'N/A')
       self.label_llm.text = self.item.get('llm_name', 'N/A')
       self.label_prompt.text = self.item.get('first_prompt', 'N/A')
-
